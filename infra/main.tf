f4439e472ed95cda5d5f7ad58dd3808803dbb310
hiroky1983 <hirockysan1983@gmail.com>
2025-01-24 23:38:13 +0900

    terraform設定

--- infra/main.tf
+++ infra/main.tf
@@ -9,5 +9,40 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  region  = var.region
+  region  = "asia-northeast1"
+}
+
+resource "google_cloud_run_v2_service" "server" {
+  name     = "cloudrun-service"
+  location = "asia-northeast1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/server/server"
+    }
+  }
+}
+
+resource "google_artifact_registry_repository" "server" {
+  location = "asia-northeast1"
+  repository_id = "server"
+  format = "DOCKER"
+  project = var.project_id
+}
+
+resource "google_storage_bucket" "tfstate" {
+  name     = "${var.project_id}-tfstate"
+  location = "asia-northeast1"
+}
+
+resource "google_storage_bucket_iam_member" "tfstate_admin" {
+  bucket = google_storage_bucket.tfstate.name
+  role   = "roles/storage.admin"
+  member = "user:hirockysan1983@gmail.com"
+}
+
+variable "project_id" {
+  description = "The ID of the project in Google Cloud"
+  type        = string
 }
